ae75e752f2acb95bceac09c4d5d739d1b58512c5
alireza <alireza_be_hi@yahoo.com>
2024-01-19 19:48:00 +0330

    cleanup producer

--- src/kafka/producer.py
+++ src/kafka/producer.py
@@ -1,5 +1,6 @@
 import random
 from json import dumps
+from typing import List
 
 from kafka import KafkaProducer
 from kafka.admin import KafkaAdminClient, NewTopic
@@ -7,9 +8,9 @@ from kafka.admin import KafkaAdminClient, NewTopic
 from config import get_settings
 
 
-def check_has_topic(topic_name: str) -> bool:
+def check_has_topic(topic_name: str, boostrap_servers: List[str]) -> bool:
     admin_client = KafkaAdminClient(
-        bootstrap_servers=['172.16.40.40:9092', '172.16.40.40:9093', '172.16.40.40:9094'],
+        bootstrap_servers=boostrap_servers,
     )
     topic_list = admin_client.list_topics()
     if topic_name in topic_list:
@@ -17,20 +18,20 @@ def check_has_topic(topic_name: str) -> bool:
     return False
 
 
-def create_topic(topic_name: str, partitions: int, replication_factor: int):
+def create_topic(topic_name: str, partitions: int, replication_factor: int, boostrap_servers: List[str]) -> None:
     admin_client = KafkaAdminClient(
-        bootstrap_servers=['172.16.40.40:9092', '172.16.40.40:9093', '172.16.40.40:9094'],
+        bootstrap_servers=boostrap_servers,
     )
     new_topic = [NewTopic(name=topic_name, num_partitions=partitions,
                           replication_factor=replication_factor)]
     admin_client.create_topics(new_topics=new_topic, validate_only=False)
 
 
-def create_producer() -> KafkaProducer | None:
+def create_producer(boostrap_servers) -> KafkaProducer | None:
     # initializing the Kafka producer``
     try:
         return KafkaProducer(
-            bootstrap_servers=['172.16.40.40:9092', '172.16.40.40:9093', '172.16.40.40:9094'],
+            bootstrap_servers=boostrap_servers,
             value_serializer=lambda x: dumps(x).encode('utf-8'),
             key_serializer=lambda x: x.encode('utf-8')
         )
@@ -43,7 +44,7 @@ def collatz_conjecture(number: int, topic: str, partition_size: int, my_producer
     step = 0
 
     data = {'Number': initial_number, 'Step': step, 'Value': number}
-    my_producer.send(topic,key=str(initial_number), value=data, partition=int(initial_number % partition_size))
+    my_producer.send(topic, key=str(initial_number), value=data, partition=int(initial_number % partition_size))
     while number != 1:
         if number % 2 == 0:
             number = number // 2
@@ -51,20 +52,24 @@ def collatz_conjecture(number: int, topic: str, partition_size: int, my_producer
             number = (number * 3) + 1
         step += 1
         data = {'Number': initial_number, 'Step': step, 'Value': number}
-    my_producer.send(topic,key=str(initial_number), value=data, partition=int(initial_number % partition_size))
+        my_producer.send(topic, key=str(initial_number), value=data, partition=int(initial_number % partition_size))
 
 
 if __name__ == "__main__":
     settings = get_settings()
-    has_topic = check_has_topic(settings.kafka_topic_name)
+    has_topic = check_has_topic(settings.kafka_topic_name, settings.bootstrap_servers)
     if not has_topic:
-        create_topic(settings.kafka_topic_name, settings.partitions, settings.replication_factor)
+        create_topic(settings.kafka_topic_name, settings.partitions, settings.replication_factor,
+                     settings.bootstrap_servers)
 
-    producer = create_producer()
+    producer = create_producer(settings.bootstrap_servers)
     if not producer:
         exit(-1)
     p_size = len(producer.partitions_for(settings.kafka_topic_name))
+    print(f'partition size: {p_size}, topic: {settings.kafka_topic_name}')
     while True:
         random_number = random.randint(1, 99999999999999)
         collatz_conjecture(number=random_number, topic=settings.kafka_topic_name, partition_size=p_size,
                            my_producer=producer)
+        producer.flush()
+    producer.close()
